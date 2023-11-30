91a0694b4aa90ff4124a1de40f6d5520a41b7e3d
vinod warnakulasooriya <116092558+vinuwa98@users.noreply.github.com>
2023-11-30 21:53:41 +0530

    Add Background Image
    I add background image like prototype

--- App.tsx
+++ App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, ImageBackground, Text, TextInput, View } from 'react-native';
 import Label from './src/components/Label';
 
 function App(): JSX.Element {
-
   return (
-    <View style={{alignItems: 'center'}}>
-      <Text
-        style={{
-          fontSize: 40,
-          color: 'red',
-        }}>
-        Hello World
-      </Text>
-      <Text style={{marginBottom: 10}}>
-        React-Native
-      </Text>
-      <Button title='Click me'/>
+    <ImageBackground
+      source={require('./src/Background.png')}
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+    >
+      <View style={{ alignItems: 'center', flex: 1, justifyContent: 'flex-end', marginBottom: 30 }}>
+        <Text style={{ fontSize: 40, color: 'red' }}>Hello World</Text>
+        <Text style={{ marginBottom: 10 }}>React-Native</Text>
+        <Button title='Click me' />
 
-      <Label>Welcome !</Label>
+        <Label>Welcome!</Label>
 
-    </View>
+        <TextInput style={{ backgroundColor: '#aaa', color: '#000', width: 200 }} />
+        <Button title="Let's Get Started" onPress={() => alert("Let's get started!")} />
+      </View>
+    </ImageBackground>
   );
 }
 
